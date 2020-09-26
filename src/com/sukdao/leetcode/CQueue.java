94f2ce0e1821d506e0247f85192e3d2391e09678
xrian <iocntop@gmail.com>
2020-09-26 22:41:28 +0800

    no message

--- src/com/sukdao/leetcode/CQueue.java
+++ src/com/sukdao/leetcode/CQueue.java
@@ -1,2 +1,96 @@
-package com.sukdao.leetcode;public class CQueue {
+/**
+ * https://leetcode-cn.com/problems/yong-liang-ge-zhan-shi-xian-dui-lie-lcof/
+ * 剑指 Offer 09. 用两个栈实现队列
+ * 双栈实现
+ * @author iocntop@gmail.com
+ */
+
+package com.sukdao.leetcode;
+
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Stack;
+
+public class CQueue {
+
+    // 题解1,  401 ms	47.4 MB
+//    Stack<Integer> a = new Stack<>();
+//    Stack<Integer> b = new Stack<>();
+//
+//    public CQueue() {
+//
+//    }
+//
+//    public void appendTail(int value) {
+//        a.push(value);
+//    }
+//
+//    public int deleteHead() {
+//        if (a.empty() && b.empty()){
+//            return -1;
+//        }
+//        if (b.empty()){
+//            while (!a.empty()){
+//                b.push(a.pop());
+//            }
+//        }else {
+//            return b.pop();
+//        }
+//
+//
+//        return b.empty() ? -1 : b.pop();
+//    }
+
+    // 题解2  66 ms	47 MB
+//    Stack<Integer> a = new Stack<>();
+//    Stack<Integer> b = new Stack<>();
+//
+//    public CQueue() {
+//
+//    }
+//
+//    public void appendTail(int value) {
+//        a.push(value);
+//    }
+//
+//    public int deleteHead() {
+//        if (a.empty() && b.empty()){
+//            return -1;
+//        }
+//        if (b.empty()){
+//            while (!a.empty()){
+//                b.push(a.pop());
+//            }
+//        }else {
+//            return b.pop();
+//        }
+//
+//
+//        return b.empty() ? -1 : b.pop();
+//    }
+
+    // 题解3   56 ms	47.9 MB   使用 Deque 替换 stack
+    Deque<Integer> a = new LinkedList<>();
+    Deque<Integer> b = new LinkedList<>();
+
+    public CQueue() {
+
+    }
+
+    public void appendTail(int value) {
+        a.push(value);
+    }
+
+    public int deleteHead() {
+        if (a.isEmpty() && b.isEmpty()) {
+            return -1;
+        }
+        if (b.isEmpty()) {
+            while (!a.isEmpty()) {
+                b.push(a.pop());
+            }
+        }
+        return b.pop();
+    }
+
 }
