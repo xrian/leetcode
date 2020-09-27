5f7ca66b0e093400aef9f2489d0b8eb017bf5554
张淞 <iocntop@gmail.com>
2020-09-27 15:31:02 +0800

    no message

--- src/com/sukdao/leetcode/LinkedListCycle.java
+++ src/com/sukdao/leetcode/LinkedListCycle.java
@@ -2,6 +2,7 @@
  * https://leetcode-cn.com/problems/linked-list-cycle/
  * 141. 环形链表
  * 快慢指针
+ *
  * @author iocntop@gmail.com
  */
 package com.sukdao.leetcode;
@@ -11,6 +12,18 @@ import com.sukdao.leetcode.entity.ListNode;
 public class LinkedListCycle {
 
     public boolean hasCycle(ListNode head) {
+        if (head != null && head.next != null) {
+            ListNode slow = head;
+            ListNode fast = head.next.next;
+            while (slow != null && fast != null && fast.next != null) {
+                if (slow.val == fast.val) {
+                    return true;
+                } else {
+                    slow = slow.next;
+                    fast = fast.next.next;
+                }
+            }
+        }
         return false;
     }
 }
