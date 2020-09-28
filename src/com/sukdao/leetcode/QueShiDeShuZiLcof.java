16bf69292aaf5e9df4acddf8186742b994a4ffb7
张淞 <iocntop@gmail.com>
2020-09-28 14:31:38 +0800

    no message

--- src/com/sukdao/leetcode/QueShiDeShuZiLcof.java
+++ src/com/sukdao/leetcode/QueShiDeShuZiLcof.java
@@ -7,39 +7,46 @@
  */
 package com.sukdao.leetcode;
 
+import java.util.Arrays;
+
 public class QueShiDeShuZiLcof {
+
+    // 解答1          0 ms	39.5 MB
+    // 二分查找
 
     public int exec(int[] nums, int start, int end) {
         int sub = end - start;
         if (sub == 0) {
             return nums[start] > start ? start : start + 1;
+        } else if (sub == 1) {
+            if (nums[start] != start) {
+                return start;
+            } else if (nums[end] != end) {
+                return end;
+            } else {
+                return end + 1;
+            }
         }
         int i = start + sub / 2 + (sub % 2 != 0 ? 1 : 0);
-        return nums[i] > i ? exec(nums, i, end) : exec(nums, start, i);
+        return nums[i] == i ? exec(nums, i, end) : exec(nums, start, i);
     }
 
     public int missingNumber(int[] nums) {
-        if (nums.length == 2) {
-            if (nums[0] != 0) {
-                return 0;
-            } else if (nums[1] != 1) {
-                return 1;
-            } else {
-                return 2;
-            }
-        }
         return exec(nums, 0, nums.length - 1);
     }
 
+    public void test(int[] nums, int num) {
+        System.out.println(num + "     " + Arrays.toString(nums));
+        System.out.println(missingNumber(nums) == num);
+    }
+
     public static void main(String[] args) {
-//        int[] nums = {0, 1};
-//        int[] nums = {1, 2};
-//        int[] nums = {1, 2};
-//        int[] nums = {0, 1, 3};
-        int[] nums = {0, 2, 3};
 
         QueShiDeShuZiLcof q = new QueShiDeShuZiLcof();
-
-        System.out.println(q.missingNumber(nums));
+        q.test(new int[]{0, 1}, 2);
+        q.test(new int[]{1, 2}, 0);
+        q.test(new int[]{0, 1, 3}, 2);
+        q.test(new int[]{0, 2, 3}, 1);
+        q.test(new int[]{0, 1, 2, 3, 4, 5, 6, 7, 9}, 8);
     }
 }
